2ae43d7944b21a447dc143f51c549ed99bb9e37c
stav alfi <stav.alfi@nice.com>
2018-04-01 08:39:22 +0300

    implemented reactive consumer and reactive producer

--- spring-cloud-stream-consumer/src/main/java/com/nice/spring/cloud/stream/consumer/SpringCloudStreamKinesisConsumerApplication.java
+++ spring-cloud-stream-consumer/src/main/java/com/nice/spring/cloud/stream/consumer/SpringCloudStreamKinesisConsumerApplication.java
@@ -7,22 +7,12 @@ package com.nice.spring.cloud.stream.consumer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.messaging.SubscribableChannel;
-
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
+import org.springframework.cloud.stream.messaging.Sink;
+import reactor.core.publisher.Flux;
 
 
-interface MyInputChannels {
-	String KINESIS_INPUT_CHANNEL = "input-channel-from-kinesis";
-
-	@Input(KINESIS_INPUT_CHANNEL)
-	SubscribableChannel kinesisInputChannel();
-}
-
-@EnableBinding(MyInputChannels.class)
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class SpringCloudStreamKinesisConsumerApplication {
 
@@ -30,16 +20,22 @@ public class SpringCloudStreamKinesisConsumerApplication {
 		SpringApplication.run(SpringCloudStreamKinesisConsumerApplication.class, args);
 	}
 
-	long startTime = System.currentTimeMillis();
-	int counter = 0;
-
-	@StreamListener(value = MyInputChannels.KINESIS_INPUT_CHANNEL)
-	public void kinesisSubscriber(String payload) {
-		String currentDate = DateTimeFormatter.ofPattern("hh:mm:ss.SSS").format(ZonedDateTime.now());
-		long currentTime = System.currentTimeMillis();
-		long delay = currentTime - this.startTime;
-		this.startTime = System.currentTimeMillis();
-		System.out.println("current time: " + currentDate + ", time from last batch: " + delay + ", messsages until now: " + counter + " - kinesis consumer received data: " + payload);
-		counter++;
+	@StreamListener(value = Sink.INPUT)
+	public void kinesisSubscriber(Flux<String> input) {
+		input.subscribe(System.out::println, Throwable::printStackTrace, () -> System.out.println("finish!"));
 	}
 }
+
+
+//	long startTime = System.currentTimeMillis();
+//	int counter = 0;
+
+//	@StreamListener(value = MyInputChannels.KINESIS_INPUT_CHANNEL)
+//	public void kinesisSubscriber(String payload) {
+//		String currentDate = DateTimeFormatter.ofPattern("hh:mm:ss.SSS").format(ZonedDateTime.now());
+//		long currentTime = System.currentTimeMillis();
+//		long delay = currentTime - this.startTime;
+//		this.startTime = System.currentTimeMillis();
+//		System.out.println("current time: " + currentDate + ", time from last batch: " + delay + ", messsages until now: " + counter + " - kinesis consumer received data: " + payload);
+//		counter++;
+//	}
